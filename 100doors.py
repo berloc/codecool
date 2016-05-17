2b91e45684188f2a1cbb5f84e677c09126db3348
berloc <berloc1987@gmail.com>
2016-05-17 11:37:26 +0200

    Add files via upload

---
+++ 100doors.py
@@ -0,0 +1,21 @@
+doors = [0]*101
+#fuggvany, ami megvizsgalja, hogy nyitva van-e az ajto és kinyitja, ha zarva
+def switch(x):
+    global doors
+    if doors[x] == 0:
+        doors[x] = 1
+    else:
+        doors[x] = 0
+
+for i in range (1,101):
+    for j in range (1, 101):
+        if j%i == 0:
+            switch(j)
+            j += 1
+        else:
+            j += 1
+    i += 1
+
+for y in range (1,101):
+    if doors[y] == 1:
+        print (y)
